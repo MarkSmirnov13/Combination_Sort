7438818d90bdcebb7667a50b420434d3248c21a2
MarkSmirnov13 <marksmirnov13@gmail.com>
2018-02-23 19:58:26 +0300

    upd

--- main.cpp
+++ main.cpp
@@ -3,68 +3,73 @@
 #include <fstream>
 
 using namespace std;
-template <typename T>
-void comb(vector<T> &sort)
+template <typename T> // declare new template T
+
+/*
+ * Function, that sorts the array in correct order
+ *
+ * @param {vector<T>} &array to current array which must be sort
+ * @returns nothing
+ */
+
+void Comb(vector<T> &array)
 {
-    int n = 0; // количество перестановок
-    double fakt = 1.2473309; // фактор уменьшения
-    double step = sort.size() - 1;
+    double fakt = 1.2473309; // factor of decreasing
+    double step = array.size() - 1; // the step of sort
 
-    while (step >= 1)
+    while (step >= 1) // while there is more than 1 in difference between two indexes
     {
-        for (int i = 0; i + step < sort.size(); ++i)
-        {
-            if (sort[i] > sort[i + step])
-            {
-                swap(sort[i], sort[i + step]);
-                n++;
-            }
-        }
-        step /= fakt;
+        for (int i = 0; i + step < array.size(); ++i) // looking for bad pairs
+            if (array[i] > array[i + step]) // if pair is not in correct order
+                swap(array[i], array[i + step]); // swap that bad pair
+        step /= fakt; // count the step using the rule of sort
     }
-    // сортировка пузырьком
-    for (int i = 0; i < sort.size() - 1; i++)
-    {
-        bool swapped = false;
-        for (int j = 0; j < sort.size() - i - 1; j++)
-        {
-            if (sort[j] > sort[j + 1]) {
-                swap(sort[j], sort[j + 1]);
-                swapped = true;
-                ++n;
-            }
-        }
 
-        if (!swapped)
-            break;
+    // if there is only 1 in difference between two indexes -> start bubble sort
+    for (int i = 0; i < array.size() - 1; i++) // looking for bad pairs
+    {
+        bool swapped = false; // if array is in correct order
+        for (int j = 0; j < array.size() - i - 1; j++) // looking for bad pairs
+            if (array[j] > array[j + 1]) // if pair is not in correct order
+            {
+                swap(array[j], array[j + 1]); // swap that bad pair
+                swapped = true; // array is in correct order
+            }
+
+        if (!swapped) // if array is in correct order
+            break; // go to main
     }
 }
 
 int main()
 {
-    ifstream Fin("input.txt");
-    vector<string> sort;
-    int a;
-    long t;
-    t = clock();
+    ifstream Fin("input.txt"); // declare and open input File
+    vector<string> array; // declare vector
+    unsigned long a; // declare number of elements
+    long t; // declare the time parameter
 
-    Fin >> a;
+    t = clock(); // set timer
 
-    sort.resize(a);
+    Fin >> a; // read number "a" from the input File
 
-    for (int i = 0; i < a; i++)
-        Fin >> sort[i];
-    Fin.close();
+    array.resize(a); // resize array to correct size
 
-    comb(sort);
+    for (int i = 0; i < a; i++) // read the array to vector from the input File
+        Fin >> array[i];
+    Fin.close(); // close input File
 
-    t = clock() - t;
-    ofstream Fout("output.txt");
-    for (int i = 0; i < a; i++)
-        Fout << sort[i] << endl;
+    Comb(array); // sort the array
 
-    Fout << t << "ms"<< endl;
+    t = clock() - t; // count the time of sort
 
-    Fout.close();
+    ofstream Fout("output.txt"); // declare and open output File
+
+    for (int i = 0; i < a; i++) // write the array in correct order
+        Fout << array[i] << endl;
+
+    Fout << t << "ms"<< endl; // write the sort time
+
+    Fout.close();// close output File
+
     return 0;
 }
